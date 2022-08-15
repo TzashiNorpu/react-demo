1af84b60e09e0c4a493c37b022aa6254168e302c
tzashinorpu <tzashinorpu@gmail.com>
2022-08-15 14:22:13 +0800

    feat: 添加 useMount 和 useDebounce 两个 hook

--- src/screens/project-lists/index.jsx
+++ src/screens/project-lists/index.jsx
@@ -2,7 +2,7 @@ import {List} from "./list";
 import {SearchPanel} from "./search-panel";
 import {useEffect, useState} from "react";
 import React from "react";
-import {cleanObject} from "utils";
+import {cleanObject, useDebounce, useMount} from "utils";
 import qs from 'qs';
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
@@ -12,21 +12,25 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
+
+  const debouncedParam = useDebounce(param, 2000);
+
   // param 改变时从接口获取数据
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param]);
-  useEffect(() => {
+  }, [debouncedParam]);
+
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json());
       }
     })
-  }, []);
+  });
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List users={users} list={list} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,48 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+// hooks  组件加载时只执行一次
+// hook 只能在其他 hook 或组件中运行
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+// debounce 原理
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param);
+//     }, delay);
+//   }
+// }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
+//   ...5s
+// 执行！
+
+// debounce 原理讲解：
+// 0s ---------> 1s ---------> 2s --------> ...
+//     一定要理解：这三个函数都是同步操作，所以它们都是在 0~1s 这个时间段内瞬间完成的；
+//     log()#1 // timeout#1
+//     log()#2 // 发现 timeout#1！取消之，然后设置timeout#2
+//     log()#3 // 发现 timeout#2! 取消之，然后设置timeout#3
+//             // 所以，log()#3 结束后，就只剩timeout#3在独自等待了
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
 };
