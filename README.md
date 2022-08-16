79b056af27f7f41058cd489f5234d4a0031579c7
tzashinorpu <tzashinorpu@gmail.com>
2022-08-16 21:09:35 +0800

    feat: add antd

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-- microservice
-  yarn add jira-dev-tool
-  npx msw init public
+### microservice
+
+- yarn add jira-dev-tool
+- npx msw init public
+
+#### CSS
+
+- yarn add antd
+
+#### CRACO
+
+- yarn add @craco/craco
+- yarn add craco-less
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,33 +1,44 @@
 import { useAuth } from "context/auth-context";
-import React, { FormEvent } from "react";
+import React from "react";
+import { Button, Form, Input } from "antd";
 
 export const LoginScreen = () => {
   const { login } = useAuth();
-  const handleSumbit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  // const handleSumbit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSumbit = (values: { username: string; password: string }) => {
+    /* event.preventDefault();
     const username = (event.currentTarget.elements[0] as HTMLInputElement)
       .value;
     const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    login({ username, password });
+      .value; 
+    login({username, password});*/
+    login(values);
   };
   return (
-    <form onSubmit={handleSumbit}>
+    <Form onFinish={handleSumbit}>
       {/*       {
         user ? <div>
           登录成功，用户名:{user?.name}
           token"{user?.token}
         </div> : null
       } */}
-      <div>
-        <label htmlFor="userName">用户名</label>
-        <input type="text" id={"userName"} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id={"password"} />
-      </div>
-      <button type={"submit"}>登录</button>
-    </form>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input placeholder={"用户名"} type="text" id={"userName"} />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input placeholder={"密码"} type="password" id={"password"} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType={"submit"} type={"primary"}>
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
