7c9bfdc0442128c7a9cac71ec1921a6bfde55b0e
tzashinorpu <tzashinorpu@gmail.com>
2022-08-18 20:59:40 +0800

    feat: hook 增删改

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -29,3 +29,35 @@ export const useProjects = (param?: Partial<Project>) => {
   }, [param]);
   return result;
 }
+
+
+
+export const useEditProject = () => {
+  const client = useHttp();
+  const {run, ...asyncResult} = useAsync();
+  const mutate = (params: Partial<Project>) => {
+    return run(client(`projects/${params.id}`, {
+      data: params,
+      method: 'PATCH'
+    }))
+  }
+  return {
+    mutate,
+    ...asyncResult
+  }
+};
+
+export const useAddProject = () => {
+  const client = useHttp();
+  const {run, ...asyncResult} = useAsync();
+  const mutate = (params: Partial<Project>) => {
+    return run(client(`projects/${params.id}`, {
+      data: params,
+      method: 'POST'
+    }))
+  }
+  return {
+    mutate,
+    ...asyncResult
+  }
+};
