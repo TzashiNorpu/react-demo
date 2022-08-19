b9bd1c13c5839c5785737190c70997e2f4f61ad6
tzashinorpu <tzashinorpu@gmail.com>
2022-08-19 18:02:16 +0800

    feat: 临时提交

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,4 +1,62 @@
-import {useCallback, useState} from "react";
+import {useCallback, useReducer, useState} from "react";
+
+const UNDO = "UNDO";
+const REDO = "REDO";
+const SET = "SET";
+const RESET = "RESET";
+
+const undoReducer = <T>(state: State<T>, action: Action<T>) => {
+  const {past, present, future} = state;
+  const {newPresent} = action;
+
+  switch (action.type) {
+    case UNDO: {
+      if (past.length === 0) return state;
+
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
+
+      return {
+        past: newPast,
+        present: previous,
+        future: [present, ...future],
+      };
+    }
+
+    case REDO: {
+      if (future.length === 0) return state;
+
+      const next = future[0];
+      const newFuture = future.slice(1);
+
+      return {
+        past: [...past, present],
+        present: next,
+        future: newFuture,
+      };
+    }
+
+    case SET: {
+      if (newPresent === present) {
+        return state;
+      }
+      return {
+        past: [...past, present],
+        present: newPresent,
+        future: [],
+      };
+    }
+
+    case RESET: {
+      return {
+        past: [],
+        present: newPresent,
+        future: [],
+      };
+    }
+  }
+  return state;
+};
 
 
 export const useUndo = <T>(initialPresent: T) => {
@@ -6,12 +64,13 @@ export const useUndo = <T>(initialPresent: T) => {
   const [present, setPresent] = useState(initialPresent);
   const [future, setFuture] = useState<T[]>([]); */
 
+  const [] = useReducer(undoReducer,{})
+
   const [state, setState] = useState({
     past: [] as T[],
     present: initialPresent as T,
     future: [] as T[]
   });
-
 
   const canUndo = state.past.length !== 0;
   const canRedo = state.future.length !== 0;
